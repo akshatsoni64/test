dfe9c2347689c0514af4f9f44a7fd036fffd3bfe
Akshat Soni <akshatsoni64@gmail.com>
2023-03-12 19:49:32 +0000

    Revert "Flask video experiment"
    This reverts commit 35b8227cb7afbc314f90985b32890cb49039fa29.

--- main.py
+++ main.py
@@ -1,34 +1,10 @@
-from flask import *
-import cv2
+from flask import Flask
+import sqlalchemy
+app = Flask(__name__)
 
-myapp = Flask(__name__)
-myapp.secret_key = "VERY_VERY_SECRET"
-
-camera = cv2.VideoCapture(-1)
-
-def gen_frames():  
-    while True:
-        success, frame = camera.read()  # read the camera 
-        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
-        # print("Success: ", success)
-        if not success:
-            break
-        else:
-            ret, buffer = cv2.imencode('.jpg', frame)
-            # print("OLD: ",  type(buffer))            
-            frame = buffer.tobytes()
-            # print("FRAME: ", type(frame))
-            yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
-
-@myapp.route('/home')
+@app.route('/')
 def index():
-    print("Ghar aagya...")
-    return render_template("index.html")
-
-@myapp.route('/video_feed')
-def video_feed():
-    print("Video aaega ab...")
-    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return "Hello"
 
 if __name__ == "__main__":
-    myapp.run(host = "0.0.0.0", port="8081")
+    app.run(host="127.0.0.1", port="8080")
